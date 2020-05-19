021c71433f202cb72e1c8a9d619803555fdc4a43
TheNinza <niks.a3198@gmail.com>
2020-05-19 13:13:50 +0530

    edited port

--- server.js
+++ server.js
@@ -197,6 +197,6 @@ app.post("/getcenteruser", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("App is listening to port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App is listening to port ${process.env.PORT}`);
 });
